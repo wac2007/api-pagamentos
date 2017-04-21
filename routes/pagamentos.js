d3d8fca68b7c5c361da2ef056954af57cdf2ea65
Wallace Coelho <wac.almeida@gmail.com>
2017-04-20 21:11:52 -0300

    Config Body Parser

--- routes/pagamentos.js
+++ routes/pagamentos.js
@@ -2,4 +2,12 @@ module.exports = (app) => {
     app.get('/pagamentos', (req, res) => {
         res.send('Funciona!');
     });
+
+    app.post('/pagamentos/pagamento', (req, res) => {
+        var pagamento = req.body;
+        console.log('processando pagamento');
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date();
+        res.send(pagamento);
+    })
 }
